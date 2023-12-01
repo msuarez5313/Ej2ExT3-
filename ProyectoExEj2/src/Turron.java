9287ec2a20ca9e84913bd88e21c367e73a5f1eb6
msuarez5313 <msuarez-barcena5313@ceuandalucia.es>
2023-12-01 19:41:25 +0100

    pero mi preferido es el de chocolate

--- ProyectoExEj2/src/Turron.java
+++ ProyectoExEj2/src/Turron.java
@@ -3,7 +3,7 @@ public class Turron {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Soy Miguel y me encatan los turrones.");
+System.out.println("Pero mi preferido es el de chocolate.");
 	}
 
 }
